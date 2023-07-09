6c1e67b107407001d2a9d1792db6c39d9e1513f6
Evgeny Barsukou <evgeny.brv@gmail.com>
2023-07-09 16:57:11 +0300

    Fixed buffer list pop

--- app/buffer.py
+++ app/buffer.py
@@ -17,9 +17,9 @@ def del_obsolete_buffer_msg(mname, buffer) -> None:
     """Delete obsolete buffer message"""
     for num, msg in enumerate(buffer):
         if time.time() - msg.timestamp > BUFFER_MSG_LIFETIME_SEC:
-            buffer(num)
+            buffer.pop(num)
             logger.warning(
-                f"{mname} Delet obsolete outbut buffer msg: "
+                f"{mname} Deleted obsolete output buffer msg: "
                 + f"chat_id={msg.chat_id=} {msg.user_name=} {msg.text=}"
             )
             return
